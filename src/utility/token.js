fbf79f58363958733084eddc5faa8aafbae7f7aa
Alex Gubskyy <aleks.gubskyy@gmail.com>
2021-03-06 17:48:50 +0100

    fix: token cut and updated

--- src/utility/token.js
+++ src/utility/token.js
@@ -1,4 +1,7 @@
-const token = process.env.VERCEL;
+const t1 = "e499de18e84d3"
+const t2 = "a6d3f05ecfea4b3"
+const t3 = "7cd4f6769c02"
 
-console.log(process.env);
+const token = t1 + t2 + t3;
+
 export default token;
